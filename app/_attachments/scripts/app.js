7814aaaf8e29ec4268c1e3798b8f854a08227f52
Unknown <wyachou95@gmail.com>
2017-11-06 15:21:37 +0100

    bugfix

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -7,10 +7,20 @@ angular.module('parkingApp', ['ngRoute'])
         .when('/home', {
             templateUrl: 'assets/views/home.html',
             controller: 'homeCtrl'
-        });
+        })
+        .otherwise({ //BUG FIX, Bootstrap etc doesn't load without .otherwise
+            redirectTo: '/home'
+          });
 })
 
 .controller('homeCtrl', function($scope) {
+	
+	$('#searchButton').on('click', function(e) { //make onClick in code, not in HTML
+		
+		$scope.color = ''; //what if I don't put this here?
+		
+		
+	});
 	
 })
 	
