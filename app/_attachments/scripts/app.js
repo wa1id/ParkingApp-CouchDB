d37bcb4c067ecf849bf39a4eb641b1d03a40d21c
Unknown <wyachou95@gmail.com>
2017-11-06 15:47:11 +0100

    correctly does GET and returns lat, lon

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -13,14 +13,39 @@ angular.module('parkingApp', ['ngRoute'])
           });
 })
 
-.controller('homeCtrl', function($scope) {
+.controller('homeCtrl', function($scope, addressSrv) {
 	
 	$('#searchButton').on('click', function(e) { //make onClick in code, not in HTML
 		
 		$scope.color = ''; //what if I don't put this here?
 		
+		var address = $('#addressText').val();
 		
+		addressSrv.getCoordinates(address).then(function(data) {
+			//console.log(data);
+			var lat = parseFloat(data.data[0].lat);
+			var lon = parseFloat(data.data[0].lon);
+			console.log(lat);
+			console.log(lon);
+		});
 	});
 	
 })
+
+.service('addressSrv', function($http, $q) {
+	this.getCoordinates = function(address) {
+		
+		var q = $q.defer();
+		var url = 'http://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(address) + '&format=json';
+		
+		$http.get(url)
+			.then(function(data) {
+				q.resolve(data);
+			}, function error(err) {
+				q.reject(err);
+			});
+		
+		return q.promise;
+	}
+});
 	
